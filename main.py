2ed01f788fcdc91e658fac7a705437747504c989
Khodatskaa <khodatskaalina@gmail.com>
2023-10-02 23:26:34 +0300

    Ex 1 was done

--- main.py
+++ main.py
@@ -1,106 +1,154 @@
+goods = ['Tomato', 4.55, 'Mango', 89.72, 'Bell pepper', 43.88, 'Grapes', 71.55, 'Cantaloupe', 102.74]
+users = {'aaaa@gmail.com': '55555'}
+admins = {'al@admin.com': 'qwerty'}
+basket = []
+
+admin_email_pattern = '@admin.com'
+
+is_login_true = False
+is_admin = False
+
 try:
-
-    products = {
-        1: {"name": "Item 1", "price": 10},
-        2: {"name": "Item 2", "price": 15},
-        3: {"name": "Item 3", "price": 20},
-        5: {"name": "Item 5", "price": 25},
-        6: {"name": "Item 6", "price": 30},
-        7: {"name": "Item 7", "price": 35},
-        8: {"name": "Item 8", "price": 40},
-        9: {"name": "Item 9", "price": 45},
-    }
-
-    user = None
-    cart = {}
-
     while True:
-        print("Menu:")
-        print("1. Create account")
-        print("2. Log in")
-        print("3. Log out")
-        print("4. Add item")
-        print("5. Delete item")
-        print("6. Change item")
-        print("7. Add item to cart")
-        print("8. View cart")
-        print("9. Complete the purchase")
-
-        choice = input("Choose an option: ")
-
-        if choice == "1":
-            username = input("Enter username: ")
+        while True:
+            login = input("Enter login: ")
             password = input("Enter password: ")
-            user = {"username": username, "password": password}
-        elif choice == "2":
-            username = input("Enter username: ")
-            password = input("Enter password: ")
-            user = {"username": username, "password": password}
-        elif choice == "3":
-            print("Exit")
-        elif choice == "4":
-            if user and user["username"] == "Admin" and user["password"] == "Admin":
-                name = input("Enter the name of the item: ")
-                price = float(input("Enter the price of the item: "))
-                product_id = max(products.keys()) + 1
-                products[product_id] = {"name": name, "price": price}
-                print(f"Item '{name}' was added {product_id}.")
-            else:
-                print("Access denied. This option is only available for administrator")
-        elif choice == "5":
-            if user and user["username"] == "Admin" and user["password"] == "Admin":
-                product_id = int(input("Enter item identifier that has to be deleted: "))
-                if product_id in products:
-                    del products[product_id]
-                    print(f"Item{product_id} was deleted")
+            if admin_email_pattern in login:
+                if login not in admins:
+                    print("This administrator does not exist. Try again")
+                    continue
+                if admins[login] == password:
+                    print("You are logged in as an administrator")
+                    is_admin = True
+                    break
                 else:
-                    print("Item with this identifier was not found")
-            else:
-                print("Access denied. This option is only available for administrator")
-        elif choice == "6":
-            if user and user["username"] == "Admin" and user["password"] == "Admin":
-                product_id = int(input("Enter item identifier that has to be changed: "))
-                if product_id in products:
-                    name = input("Enter new item name: ")
-                    price = float(input("Enter new item price: "))
-                    products[product_id] = {"name": name, "price": price}
-                    print(f"Item {product_id} was changed")
+                    print("Incorrect password. Try again")
+                    continue
+            elif login in users:
+                if users[login] == password:
+                    print(f"You are logged in as user - {login}")
+                    break
                 else:
-                    print("Item with this identifier was not found")
+                    print("Incorrect password")
             else:
-                print("Access denied. This option is only available for administrator")
-        elif choice == "7":
-            if user:
-                product_id = int(input("Enter item identifier that has to be added to cart: "))
-                if product_id in products:
-                    quantity = int(input("Enter quantity: "))
-                    if product_id not in cart:
-                        cart[product_id] = {"name": products[product_id]["name"], "quantity": quantity,
-                                            "price": products[product_id]["price"]}
+                print("User with this login does not exist")
+                print("Want to register?")
+                answer = input("Enter yes or no: ")
+                if answer.lower() == 'yes':
+                    users[login] = password
+                    print("You are successfully registered")
+                    print("You can log in")
+
+        while True:
+            if is_admin:
+                print('You are logged in as an administrator')
+                print('To log out, enter "exit"')
+                print('To add an item, enter "add"')
+                print('To delete an item, enter "del"')
+                print('To log out from the administrator mode, enter "exit admin"')
+                answer = input("Enter a command: ")
+                if answer == 'exit':
+                    exit()
+                elif answer == 'add':
+                    name = input("Enter item name: ")
+                    price = float(input("Enter item price: "))
+                    goods.append(name)
+                    goods.append(price)
+                    print("Item added")
+                elif answer == 'del':
+                    name = input("Enter item name: ")
+                    if name in goods:
+                        item_index = goods.index(name)
+                        del goods[item_index:item_index + 2]
+                        print("Item deleted")
                     else:
-                        cart[product_id]["quantity"] += quantity
-                    print(f"Item '{products[product_id]['name']}' was added to cart")
+                        print("This item does not exist")
+                elif answer == 'exit admin':
+                    is_admin = False
+                    break
                 else:
-                    print("Item with this identifier was not found")
+                    print("Invalid command. Try again.")
             else:
-                print("For adding items to cart you have to log in")
-        elif choice == "8":
-            print("Cart:")
-            for item_id, item in cart.items():
-                print(f"{item['name']} (x{item['quantity']}): {item['price']} грн.")
-            total = sum(item["quantity"] * item["price"] for item in cart.values())
-            print(f"Total: {total} грн.")
-        elif choice == "9":
-            if not cart:
-                print("Cart is empty")
-            else:
-                total = sum(item["quantity"] * item["price"] for item in cart.values())
-                print(f"To be paid: {total} грн.")
-                print("Thanks for the purchase!")
-                cart = []
-        else:
-            print("Incorrect selection. Try again")
+                counter = 0
+                for i in range(0, len(goods), 2):
+                    counter += 1
+                    print(f"{counter}: {goods[i]}\t\t${goods[i + 1]:.2f}")
+                print('What item would you like to buy?')
+                print('To log out, enter "exit"')
+                answer = input("Enter item name: ")
 
+                if answer == 'exit':
+                    print('Would you like to log out?')
+                    answer = input("Enter yes or no: ")
+                    if answer.lower() == 'yes':
+                        print('Goodbye!')
+                        exit()
+                    else:
+                        break
+                else:
+                    if answer in goods:
+                        basket.append(answer)
+                        print("Item added to cart")
+                    else:
+                        print("This item does not exist")
+
+        while True:
+            if basket:
+                print("Your Shopping Cart:")
+                total_price = 0
+
+                for item in basket:
+                    item_index = goods.index(item)
+                    item_name = item
+                    item_price = float(goods[item_index + 1])
+                    total_price += item_price
+
+                    print(f"{item_name}: ${item_price:.2f}")
+
+                print(f"Total Price: ${total_price:.2f}")
+
+                print("Menu:")
+                print("1. Add Item to Cart")
+                print("2. View Cart")
+                print("3. Calculate Total Price")
+                print("4. Complete Purchase")
+                print("5. Log Out")
+
+                user_choice = input("Enter your choice: ")
+
+                if user_choice == "1":
+                    item_to_add = input("Enter the item you want to add to your cart: ")
+                    if item_to_add in goods:
+                        basket.append(item_to_add)
+                        print(f"{item_to_add} has been added to your cart.")
+                    else:
+                        print("Item not found in the list.")
+                elif user_choice == "2":
+                    print("Your Shopping Cart:")
+                    for item in basket:
+                        item_index = goods.index(item)
+                        item_name = item
+                        item_price = float(goods[item_index + 1])
+                        print(f"{item_name}: ${item_price:.2f}")
+                    print(f"Total Price: ${total_price:.2f}")
+                elif user_choice == "3":
+                    print(f"Total Price: ${total_price:.2f}")
+                elif user_choice == "4":
+                    if basket:
+                        print(f"Total Price: ${total_price:.2f}")
+                        print("Thank you for your purchase!")
+                        basket = []
+                    else:
+                        print("Your cart is empty.")
+                elif user_choice == "5":
+                    print("Logging out...")
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+            else:
+                break
+
+    print("Goodbye!")
 
 except Exception as e:
     print(e)
